25eefe7b689a2ced0a18514330ff6b9c1105c5e3
Ogheneogaga Fidelis Agi <aogaga@gmail.com>
2020-07-03 05:27:31 -0500

    added code to run palindrone

--- linkedlist/PalindromeChallange.java
+++ linkedlist/PalindromeChallange.java
@@ -1,6 +1,21 @@
 class PalindromeChallange{
 	public static void main(String [] args){
 		 DoublyLinkedList<Integer> list1 = new DoublyLinkedList<Integer>();
+		list1.insertAtEnd(1);
+		list1.insertAtEnd(2);
+		list1.insertAtEnd(2);
+		list1.insertAtEnd(1);
+		System.out.print("1st ");
+		list1.printList();
+		DoublyLinkedList<Integer> list2 = new DoublyLinkedList<Integer>();
+		list2.insertAtEnd(6);
+		list2.insertAtEnd(8);
+		list2.insertAtEnd(6);
+		list2.insertAtEnd(6);
+		System.out.print("2nd ");
+		list2.printList();
+		System.out.println("Is 1st list a palindrome?  " + isPalindrome(list1));
+		System.out.println("Is 2nd list a palindrome?  " + isPalindrome(list2));
 		 
 	}
 
