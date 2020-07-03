bd8234a99728210717d6c7ba27c73c2cdb9989e8
Ogheneogaga Fidelis Agi <aogaga@gmail.com>
2020-07-03 05:03:11 -0500

    fix NthElementChallenge

--- linkedlist/NthElementChallenge.java
+++ linkedlist/NthElementChallenge.java
@@ -1,8 +1,12 @@
+package linkedlist;
+
 class NthElementChallenge{
 	
 	public static void main(String [] args){
 		
 		SinglyLinkedList<Integer> sll = new SinglyLinkedList<Integer>();
+
+
 		 sll.printList(); //list is empty
 		   System.out.println("3rd element from end : " + nthElementFromEnd(sll, 3)); //will return null
 
@@ -11,6 +15,7 @@ class NthElementChallenge{
 		   }
 
 		    sll.printList();
+		System.out.println(" Size : " + sll.size);
 			System.out.println("3rd element from end : " + nthElementFromEnd(sll, 3)); //will return 4
 			System.out.println("10th element from end : " + nthElementFromEnd(sll, 10));//will return null
 
@@ -20,18 +25,22 @@ class NthElementChallenge{
 
 	public static <T> Object nthElementFromEnd(SinglyLinkedList<T> list, int n){
 	
-		int pfromBehind = list.size - n;
+		int pfromBehind = (list.size) - n;
 
+	//	System.out.println(" size from behind" + pfromBehind);
+		int count = 0;
 		if(pfromBehind < 0 ){
-			return -1;
+			return null;
 		}
 
-		SinglyLinkedList<T>.Node  current = list.headNod;
+		SinglyLinkedList<T>.Node  current = list.headNode;
 
 		while(current != null){
-			if(pfromBehind == 0){
+			if(count == pfromBehind){
+//				System.out.println(count + " - " + current.data);
 				return current.data;
 			}
+			count++;
 			current = current.nextNode;
 		}
 		return null;
